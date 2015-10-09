caa0dae39131995da931bf5df91ec8c3dc854b7b
Techcable <Techcable@outlook.com>
2015-10-09 15:12:04 -0700

    Return AIR for empty chunk sections

--- prepare-build.sh
+++ prepare-build.sh
@@ -2,7 +2,7 @@ cd $(dirname "$0")
 
 ./init.sh
 
-if [ ! -d work/nms-src ]; then
+if [ ! -d work/nms-src/net/minecraft/server ]; then
 echo "NMS Sources not found"
 echo "Remapping Vanilla to NMS"
 ./remap-nms.sh
