2be23ace4a0e187640a44974d53160ee888b3915
Zach Brown <Zbob750@live.com>
2016-04-23 14:22:12 -0700

    Disable, Enable GPG signs as needed Pre, Post git am
    Disable GPG signing before AM, slows things down and doesn't play nicely.
    There is also zero rational or logical reason to do so for these sub-repo AMs.
    It's re-enabled (if needed) immediately after, pass or fail.
    
    Given GitHub's recent push for GPG signing, and our own testing, this is not
    only helpful, but necessary.

--- newApplyPatches.sh
+++ newApplyPatches.sh
@@ -2,6 +2,7 @@
 
 PS1="$"
 basedir=`pwd`
+gpgsign=$(git config commit.gpgsign)
 echo "Rebuilding Forked projects.... "
 
 function applyPatch {
@@ -29,12 +30,31 @@ function applyPatch {
         echo "  Something did not apply cleanly to $target."
         echo "  Please review above details and finish the apply then"
         echo "  save the changes with rebuildPatches.sh"
+        enableCommitSigningIfNeeded
         exit 1
     else
         echo "  Patches applied cleanly to $target"
     fi
 }
 
+function enableCommitSigningIfNeeded {
+    if [[ "$gpgsign" == "true" ]]; then
+        echo "Re-enabling GPG Signing"
+        # Yes, this has to be global
+        git config --global commit.gpgsign true
+    fi
+}
+
+# Disable GPG signing before AM, slows things down and doesn't play nicely.
+# There is also zero rational or logical reason to do so for these sub-repo AMs.
+# Calm down kids, it's re-enabled (if needed) immediately after, pass or fail.
+if [[ "$gpgsign" == "true" ]]; then
+    echo "_Temporarily_ disabling GPG signing"
+    git config --global commit.gpgsign false
+fi
+
 applyPatch Bukkit Spigot-API HEAD && applyPatch CraftBukkit Spigot-Server patched
 applyPatch Spigot-API PaperSpigot-API HEAD && applyPatch Spigot-Server PaperSpigot-Server HEAD
 applyPatch PaperSpigot-API TacoSpigot-API HEAD && applyPatch PaperSpigot-Server TacoSpigot-Server HEAD
+
+enableCommitSigningIfNeeded
