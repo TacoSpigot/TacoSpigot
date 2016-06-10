590c2443bd2b65bd7bf0ef2c966c1267b01b7335
Techcable <Techcable@techcable.net>
2016-06-10 12:21:13 -0700

    Fix static event methods (like ChestShop uses)
    Fixes #22

--- rebuildPatches.sh
+++ rebuildPatches.sh
@@ -26,17 +26,19 @@ function cleanupPatches {
 
 function savePatches {
     what=$1
+    what_name=$(basename $what) # TacoSpigot - add a seperate 'name' of what, for situations where 'what' contains a slash
     target=$2
     echo "Formatting patches for $what..."
     cd "$basedir/$target"
-    git format-patch --no-stat -N -o "$basedir/${what}-Patches/" upstream/upstream >/dev/null
+    git format-patch --no-stat -N -o "$basedir/${what_name}-Patches/" upstream/upstream >/dev/null
     cd "$basedir"
-    git add -A "$basedir/${what}-Patches"
-    cleanupPatches "$basedir/${what}-Patches"
-    echo "  Patches saved for $what to $what-Patches/"
+    git add -A "$basedir/${what_name}-Patches"
+    cleanupPatches "$basedir/${what_name}-Patches"
+    echo "  Patches saved for $what to $what_name-Patches/"
 }
+
 if [ "$1" == "clean" ]; then
-	rm -rf Spigot-*-Patches
+	rm -rf PaperSpigot-*-Patches
 fi
-savePatches PaperSpigot-API TacoSpgiot-API
+savePatches PaperSpigot-API TacoSpigot-API
 savePatches PaperSpigot-Server TacoSpigot-Server
