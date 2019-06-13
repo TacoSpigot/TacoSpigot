576b16bda72b83af78bd30d90aed6457203886bf
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-06-13 15:34:28 -0700

    Other submodules also have 1.13.2 versions

--- prepare-build.sh
+++ prepare-build.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 git submodule update --recursive --init && \
-cd Paper/work/BuildData && \
+# echo "Updating Paper..." && \
+cd Paper && \
+# git checkout ver/1.12.2 && \
+echo "Updating BuildData..." && \
+cd work/BuildData && \
 git checkout 8eaf2804afba334ca73c3e190cf092064c631692 && \
+echo "Updating Bukkit..." && \
+cd ../Bukkit && \
+git checkout 987016eada9da174c27b90126759a73eb21bdf76 && \
+echo "Updating CraftBukkit..." && \
+cd ../CraftBukkit && \
+git checkout version/1.13.2 && \
+# echo "Updating Paperclip..." && \
+# cd ../Paperclip && \
+# git checkout ver/1.12.2 && \
 cd ../../.. && \
 ./remap.sh && ./decompile.sh && ./init.sh && ./applyPatches.sh
 
