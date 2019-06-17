244fed8b3f62fd0ed2ff0d41855e76f13a1165b6
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-06-16 20:51:53 -0700

    Determine which remap.sh is failing

--- remap.sh
+++ remap.sh
@@ -15,7 +15,7 @@ jarpath=$workdir/Minecraft/$minecraftversion/$minecraftversion
 minecrafturl=https://theseedmc.com/mirrors/vanilla_1.13.2.jar
 useragent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
 
-echo "Downloading unmapped vanilla jar from $minecrafturl to $jarpath..."
+echo "[TacoSpigot/remap.sh] Downloading unmapped vanilla jar from $minecrafturl to $jarpath..."
 if [ ! -f  "$jarpath.jar" ]; then
     mkdir -p "$workdir/Minecraft/$minecraftversion"
     curl -A "$useragent" -s -o "$jarpath.jar" "$minecrafturl"
@@ -29,55 +29,55 @@ fi
 if [[ "$OSTYPE" == "darwin"* ]]; then
    shopt -s expand_aliases
    alias md5sum='md5 -r'
-   echo "Using an alias for md5sum on OS X"
+   echo "[TacoSpigot/remap.sh] Using an alias for md5sum on OS X"
 fi
 
 checksum=$(md5sum "$jarpath.jar" | cut -d ' ' -f 1)
 if [ "$checksum" != "$minecrafthash" ]; then
-    echo "The MD5 checksum of the downloaded server jar ($checksum) does not match the work/BuildData hash ($minecrafthash)."
+    echo "[TacoSpigot/remap.sh] The MD5 checksum of the downloaded server jar ($checksum) does not match the work/BuildData hash ($minecrafthash)."
     exit 1
 fi
 
-echo "Applying class mappings..."
+echo "[TacoSpigot/remap.sh] Applying class mappings..."
 if [ ! -f "$jarpath-cl.jar" ]; then
     if [ ! -f "$classmappings" ]; then
-        echo "Class mappings not found!"
+        echo "[TacoSpigot/remap.sh] Class mappings not found!"
         exit 1
     fi
     java -jar work/BuildData/bin/SpecialSource-2.jar map -i "$jarpath.jar" -m "$classmappings" -o "$jarpath-cl.jar"
     if [ "$?" != "0" ]; then
-        echo "Failed to apply class mappings."
+        echo "[TacoSpigot/remap.sh] Failed to apply class mappings."
         exit 1
     fi
 fi
 
-echo "Applying member mappings..."
+echo "[TacoSpigot/remap.sh] Applying member mappings..."
 if [ ! -f "$jarpath-m.jar" ]; then
     if [ ! -f "$membermappings" ]; then
-        echo "Member mappings not found!"
+        echo "[TacoSpigot/remap.sh] Member mappings not found!"
         exit 1
     fi
     java -jar work/BuildData/bin/SpecialSource-2.jar map -i "$jarpath-cl.jar" -m "$membermappings" -o "$jarpath-m.jar"
     if [ "$?" != "0" ]; then
-        echo "Failed to apply member mappings."
+        echo "[TacoSpigot/remap.sh] Failed to apply member mappings."
         exit 1
     fi
 fi
 
-echo "Creating remapped jar..."
+echo "[TacoSpigot/remap.sh] Creating remapped jar..."
 if [ ! -f "$jarpath-mapped.jar" ]; then
     java -jar work/BuildData/bin/SpecialSource.jar --kill-lvt -i "$jarpath-m.jar" --access-transformer "$accesstransforms" -m "$packagemappings" -o "$jarpath-mapped.jar" 1>/dev/null
     if [ "$?" != "0" ]; then
-        echo "Failed to create remapped jar."
+        echo "[TacoSpigot/remap.sh] Failed to create remapped jar."
         exit 1
     fi
 fi
 
-echo "Installing remapped jar..."
+echo "[TacoSpigot/remap.sh] Installing remapped jar..."
 cd work/CraftBukkit # Need to be in a directory with a valid POM at the time of install.
 mvn install:install-file -q -Dfile="$jarpath-mapped.jar" -Dpackaging=jar -DgroupId=org.spigotmc -DartifactId=minecraft-server -Dversion="$minecraftversion-SNAPSHOT"
 if [ "$?" != "0" ]; then
-    echo "Failed to install remapped jar."
+    echo "[TacoSpigot/remap.sh] Failed to install remapped jar."
     exit 1
 fi
 
